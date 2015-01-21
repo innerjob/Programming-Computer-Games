8170d50724a6ae19fbff1097538cdd4db69b8c14
shadow1349 <shadow1349@gmail.com>
2015-01-21 10:00:41 -0600

    Updates
    Added classes for circles and lines

--- Assignment1/src/Assignment1.java
+++ Assignment1/src/Assignment1.java
@@ -12,20 +12,16 @@ public class Assignment1
 {
 	private static JFrame frame;
 	
+	//Store Circles And Lines In An Array
+	private Circle[] circles = new Circle[2];
+	private Line[] lines = new Line[2];
 	
-	public static void paintCircles(Graphics g)
+	public static void paintComponent(Graphics g)
 	{
+		g.setColor(Color.BLUE);
 		for(int i = 0; i <= 3; i++)
 		{
 			//paint the circles on the screen
-		}
-	}
-	
-	public static void paintLines(Graphics g)
-	{
-		for (int i = 0; i <=3; i++)
-		{
-			//paint the lines
 		}
 	}
 	
@@ -36,10 +32,39 @@ public class Assignment1
 		frame.setBounds(100,100,500,500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Graphics g = null;
-		Graphics g1 = null;
-		paintCircles(g);
-		paintLines(g1);
+		paintComponent(g);
 		
 	}
 
+}
+
+//Create A Circle
+class Circle 
+{
+	//Default Constructor
+	public Circle()
+	{
+		
+	}
+	
+	//Constructor 
+	public Circle(int x, int y)
+	{
+		
+	}
+}
+
+//Create A Line
+class Line
+{
+	//Default Constructor
+	public Line()
+	{
+		
+	}
+	//Constructor 
+	public Line(int x, int y)
+	{
+		
+	}
 }
