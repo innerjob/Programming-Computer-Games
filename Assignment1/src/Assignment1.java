aca52a1291ea5c56a920542e532c6705d06e4c9b
shadow1349 <shadow1349@gmail.com>
2015-01-21 12:16:47 -0600

    Updates

---
+++ Assignment1/src/Assignment1.java
@@ -0,0 +1,58 @@
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+
+import javax.swing.*;
+
+
+public class Assignment1 extends JPanel{
+
+	private JFrame frame;
+	private JPanel panel;
+	private Graphics gr;
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setColor(Color.RED);
+		g2d.fillOval(0, 0, 30, 30);
+		g2d.drawOval(0, 50, 30, 30);		
+		g2d.fillRect(50, 0, 30, 30);
+		g2d.drawRect(50, 50, 30, 30);
+
+		g2d.draw(new Ellipse2D.Double(0, 100, 30, 30));
+	}
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Assignment1 window = new Assignment1();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Assignment1() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel = new JPanel();
+		
+		paint(gr);
+		
+	}
+
+}
