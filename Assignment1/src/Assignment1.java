63be7a22d3b5fc10683fa2fad9a27cfa7f6b7e23
Sam Redmond <shadow1349@gmail.com>
2015-01-23 01:40:59 -0600

    FINISHED

--- Assignment1/src/Assignment1.java
+++ Assignment1/src/Assignment1.java
@@ -6,37 +6,78 @@ import java.awt.*;
 
 public class Assignment1 
 {
-	private static Graphics graph;
-	
-	public static void paintComponent(Graphics g)
-	{
-		
-		Random rand = new Random();
-		ArrayList<Ball> balls= new ArrayList<Ball>();
-		for(int i = 0; i < 10; i++)
-		{
-			balls.add(new Ball(rand.nextInt(100), rand.nextInt(100), 40, 40));
-		}
-		for(Ball b : balls)
-			b.paint(graph);
-	}
-	
 	public static void main(String[] args)
 	{
 		JFrame frame = new JFrame("Sam Redmond: Assignment 1");
-		JPanel panel = new JPanel();
-		
-		paintComponent(graph);
-		
-		
-		
-		
-		
 		frame.setVisible(true);
 		frame.setBounds(100,100,500,500);
 		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		panel.setBackground(Color.WHITE);
-		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		PANEL p = new PANEL();
+		frame.add(p, BorderLayout.CENTER);
 	}
+}
+
+class PANEL extends JPanel
+{
+
+	int panelSize = 500;
+	
+	Random rand = new Random();
+	
+	ArrayList<Ball> balls = new ArrayList<Ball>();
+	ArrayList<Line> lines = new ArrayList<Line>();
+	
+	private Graphics graph;
+	private Image graphImage;
+	
+//	Ball b;
+	
+	public PANEL()
+	{
+		super();
+		
+		//Add Balls to the list
+		for(int i = 0; i < 10; i++)
+		{
+			balls.add(new Ball(rand.nextInt(500), rand.nextInt(500), 50, 50));
+		}
+		
+		//Add Lines To The List
+		for(int i = 0; i < 10; i++)
+		{
+			lines.add(new Line(rand.nextInt(500),rand.nextInt(500),rand.nextInt(500),rand.nextInt(500)));
+		}
+		
+		setPreferredSize(new Dimension(panelSize, panelSize));
+	}
+	
+	protected void paintComponent(Graphics g)
+	{
+		if(graph == null)
+		{
+			graphImage = createImage(panelSize, panelSize);
+			graph = graphImage.getGraphics();
+		}
+		
+		graph.setColor(Color.white);
+		graph.fillRect(0, 0, 500, 500);
+		
+		//paint the balls
+		for(Ball b : balls)
+		{
+			b.paint(graph);
+		}
+		
+		for(Line l : lines)
+		{
+			graph.drawLine(rand.nextInt(500), rand.nextInt(500), rand.nextInt(500), rand.nextInt(500));
+		}
+		
+//		b.paint(graph);
+		
+		g.drawImage(graphImage, 0, 0, null);
+	}
+	
 }
